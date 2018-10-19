006b0260deb279a916374bd934f742cda705d5e2
kailasj-webonise <kailas.joshi@weboniselab.com>
2018-10-19 16:56:11 +0530

    Changes done in first assignment

--- assignment2.sql
+++ assignment2.sql
@@ -12,6 +12,8 @@ CREATE TABLE works (emp_id int,dept_id int,pct_time int,PRIMARY KEY(emp_id,dept_
 union
  select e1.emp_name,e1.age from emp e1,works w1,dept d1 where e1.emp_id=w1.emp_id and w1.dept_id=d1.dept_id and d1.dept_name='software';
 
+2.select dept.manager_id , GROUP_CONCAT(emp.name)from emp,works,dept WHERE emp.id=works.emp_id and works.dept_id= dept.id GROUP BY dept.manager_id;
+
 4.select e.emp_name from emp e,works w,dept d where e.emp_id=w.emp_id and w.dept_id=d.dept_id and e.salary > all(select max (d2.budget) from dept d2 where d2.dept_id=d.dept_id);
 
 
